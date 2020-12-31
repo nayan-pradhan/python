138b5d6e7dcf3c0ec74fc2d26d6706f393b28bf5
Nayan <nayan.pradhan@hotmail.com>
2020-12-31 19:42:55 +0100

    New Year's Countdown

--- countdown.py
+++ countdown.py
@@ -1,12 +1,11 @@
 import datetime
 import time
 
-loop_bool = True
-while loop_bool:
-	current_time = time.strftime("%H:%M:%S", time.localtime())
-	if (current_time == "19:27:30"):
-		print("TRUE")
-		loop_bool = False
-	else:
-		print("FALSE")
-	print(current_time)
+print ("----------x----------x----------x----------x----------x----------")
+print ("New Year's Countdown")
+print ("Press CTRL+C to exit")
+print ("----------x----------x----------x----------x----------x----------")
+
+try:
+
+except:
